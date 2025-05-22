9f47ef0b9c535043d9b898b149bc84a59bba35b8
ShirobokovDA <shirobockow.dim@yandex.ru>
2025-05-22 15:49:46 +0300

    auth test

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,66 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173/",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173/",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class DataLogIn(BaseModel):
+    email: str
+    password: str
+
+
+class DataSignUp(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+db_auth = {}
+
+
+@app.post('/api/log-in')
+def login(data: DataLogIn):
+    print("Вход с:", data.email, data.password)
+    if data.email in db_auth:
+        if db_auth[data.email] == data.password:
+            return {"message": "Вход выполнен успешно"}
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Неверный пароль"
+            )
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Пользователь не найден"
+        )
+    
+
+@app.post('/api/sign-up', status_code=status.HTTP_201_CREATED)
+def signup(data: DataSignUp):
+    print("Регистрация с:", data.name, data.email, data.password)
+    if data.email in db_auth:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email уже зарегистрирован"
+        )
+    else:
+        db_auth[data.email] = data.password
+        return {"message": "Регистрация выполнена успешно"}
